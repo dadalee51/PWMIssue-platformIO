23e7ca7704dd290a5d7c6d48aa93bd05f7b6bc7e
dadalee51 <dadalee51@gmail.com>
2024-05-17 09:27:37 +1000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <Arduino.h>
-
 void setup() {
   pinMode(PIN_PB5, OUTPUT);
   analogWrite(PIN_PB5, 0xF0);
 }
-
 void loop(){
-    #ifdef ARDUINO_MAIN
     analogWrite(PIN_PB5, 0x80);
     delay(100);
-    analogWrite(PIN_PB5, 0x70);
+    analogWrite(PIN_PB5, 0xAF);
     delay(100);
-    #endif
+    
     delay(10);
     
 };
