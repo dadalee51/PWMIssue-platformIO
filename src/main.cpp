c032cec4d3b960ac2d8aedb59bf925b6270482b3
dadalee51 <dadalee51@gmail.com>
2024-05-17 00:09:54 +1000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -33,10 +33,8 @@ void setup() {
 
     TCA0.SPLIT.CTRLD = TCA_SPLIT_SPLITM_bm; // Enable split mode
     TCA0.SPLIT.LPER = 0xFE; // Set period for lower byte
-    TCA0.SPLIT.HPER = 0xFE; // Set period for lower byte
-    
+    TCA0.SPLIT.HPER = 0xFE; // Set period for high byte
     TCA0.SPLIT.CTRLA = TCA_SPLIT_CLKSEL_DIV1024_gc | TCA_SPLIT_ENABLE_bm; // Clock select and enable
-
     TCA0.SPLIT.CTRLB = TCA_SPLIT_HCMP0EN_bm |TCA_SPLIT_LCMP2EN_bm | TCA_SPLIT_LCMP1EN_bm; 
 //Setting the HCMPnEN bit in the FRQ or PWM Waveform Generation mode of operation will override the port output
 //register for the corresponding WO[n+3] pin. 
@@ -53,6 +51,9 @@ void setup() {
       * LCMP0 - PB0 / 3
         LCMP1 - PB1 / 4
         LCMP2 - PB2 / 5 - PB5 - blue
+        HCMP0 - PA3 Red
+        HCMP1 - PA4
+        HCMP2 - PA5
 
      * https://github.com/SpenceKonde/megaTinyCore/discussions/384
      * 
@@ -65,15 +66,15 @@ void setup() {
 char data=0;
 
 void loop() {
-    if(data>127)data=0; 
+    if(data>0xFEFEFE)data=0; 
     data+=1;
     //TCA0.SINGLE.CMP0 = data;
     //TCA0.SINGLE.CMP1 = data; //GREEN in normal mode.
     //TCA0.SINGLE.CMP2 = data;
     //TCA0.SPLIT.LCMP1 = data;  //Green as well?
-    TCA0.SPLIT.LCMP2 = data; //blue
+    //TCA0.SPLIT.LCMP2 = data; //blue
     //TCA0.SPLIT.HCMP0 = data; //RED!!!
-    
+    show_RGB(data);
     delay(1);
     // TCA0.SPLIT.LCMP1 = 0xFE; //green
     // TCA0.SPLIT.HCMP0 = 0xFE; //red
@@ -85,7 +86,7 @@ void loop() {
     // delay(2000);
 }
 void show_RGB(long val){
-    analogWrite(PIN_PA3,(val>>16) & 0xFE);
-    analogWrite(PIN_PB4,(val>>8)  & 0xFE);
-    analogWrite(PIN_PB5,val       & 0xFE);
+    analogWrite(PIN_PA3,(val>>16) & 0xFF);
+    analogWrite(PIN_PB4,(val>>8)  & 0xFF);
+    analogWrite(PIN_PB5,val       & 0xFF);
 }
